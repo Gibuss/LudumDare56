eb82d94e42fc4be298767334a4d4fec0c3c03d34
NicoDuchesne <nicolas.duchesne@edu.devinci.fr>
2024-10-07 17:19:05 +0200

    final prototype

--- Assets/Scripts/borderLife/defenderLife.cs
+++ Assets/Scripts/borderLife/defenderLife.cs
@@ -9,7 +9,13 @@ public class defenderLife : MonoBehaviour
     public RectTransform lifeBarObject;
     public Vector3 lifeBarOffset = new Vector3(0, 50, 0);
     public Camera mainCamera;
+    public GameObject endMenu;
 
+    public void Awake()
+    {
+        endMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
     public void Start()
     {
         currentLife = MaxLife;
@@ -50,5 +56,7 @@ public class defenderLife : MonoBehaviour
     {
         Destroy(gameObject);
         Debug.Log("Le défenseur est mort");
+        endMenu.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
