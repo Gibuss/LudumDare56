0228e402216867c169d8bcfcf6be39c4d683902a
Gibus <samy.azhar@outlook.com>
2024-10-07 14:21:05 +0200

    Mod

--- Assets/Scripts/Managers/TurretUpgradeManager.cs
+++ Assets/Scripts/Managers/TurretUpgradeManager.cs
@@ -15,7 +15,20 @@ public class TurretUpgradeManager : MonoBehaviour
     private void Start()
     {
         turret = GetComponent<Turret>();
+        if (turret == null)
+        {
+            Debug.LogError("Le composant pas trouvé");
+            return;
+        }
+
         turretData = Resources.Load<TurretData>("TurretData");
+        if (turretData == null)
+        {
+            Debug.LogError("TurretData introuvable dans Resources.");
+            return;
+        }
+
+        Debug.Log($"Nom de la tourelle : {turretName}, Niveau actuel : {currentLevel}");
 
         ApplyUpgrade(currentLevel);
     }
@@ -29,12 +42,18 @@ public class TurretUpgradeManager : MonoBehaviour
         if (currentLevel < 3)
         {
             currentLevel++;
+            Debug.Log($"Amélioration de la tourelle. Nouveau niveau : {currentLevel}");
             ApplyUpgrade(currentLevel);
+        }
+        else
+        {
+            Debug.Log("Niveau maximum atteint. Impossible d'améliorer");
         }
     }
 
     private void ApplyUpgrade(int level)
     {
+        Debug.Log($"Application de l'amélioration pour la tourelle '{turretName}' au niveau {level}");
         TurretUpgrade upgrade = turretData.GetUpgrade(turretName, level);
 
         if (upgrade != null)
@@ -43,7 +62,12 @@ public class TurretUpgradeManager : MonoBehaviour
             damage = (int)upgrade.damage;
             damageInterval = upgrade.damageInterval;
 
+            Debug.Log($"Amélioration trouvée : Portée = {targetingRange}, Dégâts = {damage}, Intervalle = {damageInterval}");
             turret.SetStats(targetingRange, damage, damageInterval);
+        }
+        else
+        {
+            Debug.LogWarning($"Aucune amélioration trouvée pour la tourelle '{turretName}' niveau {level}.");
         }
     }
 }
